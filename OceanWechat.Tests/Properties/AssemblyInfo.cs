7fc8cd5afc7dcec2d56e9575c4e8c1d302cddab9
wowhy <hongy19890331@outlook.com>
2014-09-27 11:01:41 +0800

    create projects

--- OceanWechat.Tests/Properties/AssemblyInfo.cs
+++ OceanWechat.Tests/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// 有关程序集的常规信息通过下列特性集
+// 有关程序集的常规信息是通过以下项进行控制的
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("OceanWechat.Tests")]
@@ -10,17 +10,17 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OceanWechat.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("版权所有(C)  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// 将 ComVisible 设置为 false 会使此程序集中的类型
+// 将 ComVisible 设置为 false 将使此程序集中的类型
 // 对 COM 组件不可见。如果需要
 // 从 COM 访问此程序集中的某个类型，请针对该类型将 ComVisible 特性设置为 true。
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
-[assembly: Guid("d5aac64b-5a08-41d7-9a5b-e7454817f5e7")]
+[assembly: Guid("94df4fc2-8499-45e2-b456-ddc0c0a19124")]
 
 // 程序集的版本信息由下列四个值组成:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      内部版本号
 //      修订版本
 //
-// 可以指定所有值，也可以使用“内部版本号”和“修订号”的默认值，
+// 你可以指定所有值，也可以让修订版本和内部版本号采用默认值，
 // 方法是按如下所示使用 "*":
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
