ee8462cd2e75a402471aa16ad719e8dbe4cbede2
wowhy <hongy19890331@outlook.com>
2014-10-04 22:15:21 +0800

    test

---
+++ OceanWechat.Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿namespace OceanWechat.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+
+    public class UserController : Controller
+    {
+        public string Index()
+        {
+            return "";
+        }
+    }
+}
