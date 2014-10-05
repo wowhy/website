8f7b30591edb4e1d5f93ee49ad81fee9b239d8c0
wowhy <hongy19890331@outlook.com>
2014-10-05 14:21:23 +0800

    xx

--- OceanWechat.Models/IdentityModels.cs
+++ OceanWechat.Models/IdentityModels.cs
@@ -3,14 +3,13 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
-using OceanWechat.Models.Identity;
 
 namespace OceanWechat.Models
 {
     // 可以通过向 ApplicationUser 类添加更多属性来为用户添加配置文件数据。若要了解详细信息，请访问 http://go.microsoft.com/fwlink/?LinkID=317594。
-    public class ApplicationUser : GuidUser
+    public class ApplicationUser : IdentityUser
     {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(GuidUserManager<GuidUser> manager)
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // 请注意，authenticationType 必须与 CookieAuthenticationOptions.AuthenticationType 中定义的相应项匹配
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
@@ -19,7 +18,7 @@ namespace OceanWechat.Models
         }
     }
 
-    public class ApplicationDbContext : GuidDbContext<ApplicationUser>
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
@@ -29,6 +28,14 @@ namespace OceanWechat.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .Property(k => k.Id);
         }
     }
 }
